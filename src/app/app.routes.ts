99eea25f32b1522cac2825363a6dc2b220d6b75e
Lorenzo <lorenzochavezxd@gmail.com>
2024-10-26 20:44:40 -0600

    Proyecto inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: AuthModule},
+];
