951ef62ee344777629ac2b0257a22e14078547a1
Lorenzo <lorenzochavezxd@gmail.com>
2024-10-27 21:13:40 -0600

    Lista de productos y cards

--- src/app/general/card/card.component.ts
+++ src/app/general/card/card.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  title : string = "Titulo";
+  description: string = "Descripcion detallada del juego";
+  releaseDate: string = "2024-12-10";
+  image: string = "url";
+  rating: number = 5;
+  downloads: number = 2000;
+  comingSoon: boolean = false;
 
+  
 }
--- src/app/productos/lista-productos/lista-productos.component.ts
+++ src/app/productos/lista-productos/lista-productos.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CardComponent } from '../../general/card/card.component';
 
 @Component({
   selector: 'app-lista-productos',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './lista-productos.component.html',
   styleUrl: './lista-productos.component.css'
 })
